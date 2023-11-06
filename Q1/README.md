1e0e46edf779f5b7aab88eeb54d0ece266130837
abhiamola <abhiamola@gmail.com>
2023-11-05 22:11:14 -0500

    Q1 and Q2 update

---
+++ Q1/README.md
@@ -0,0 +1,30 @@
+# Test Q1
+
+Program is written in Java and hecks if two lines, represented by their coordinates, overlap or not. It achieves this by defining a method doLinesOverlap that takes two arrays of integers, each containing two coordinates for a line. Inside this method, it checks if the lines overlap by comparing the x-coordinates. The main method of the program prompts the user to enter coordinates for two lines, calls the doLinesOverlap method, and prints true if the lines overlap and false otherwise.
+
+To use the provided Java program via the console, follow these steps:
+
+Compile the Java Program:
+First, make sure you have the Java Development Kit (JDK) installed on your system.
+- Save the Java code into a file, for example, Test_Q1.java.
+- Open a command prompt or terminal window.
+- Navigate to the directory where Test_Q1.java is saved using the cd command (e.g., cd path/to/your/java/file).
+- Compile the Java file by typing: javac Test_Q1.java
+- This will compile the Java source code and create a Test_Q1.class file.
+
+Run the Program:
+After compiling successfully, run the program with the following command: java Test_Q1
+- This will execute the program and the console will display: Enter coordinates of first line (Separated by a space):
+- Enter the coordinates for the first line, separated by a space (for example, 1 5) and press Enter.
+- The console will then display: Enter coordinates of second line (Separated by a space):
+- Enter the coordinates for the second line, separated by a space (for example, 3 7) and press Enter.
+
+View the Output:
+- The program will calculate if the lines represented by the entered coordinates overlap or not.
+- The output, either true (if lines overlap) or false (if lines do not overlap), will be displayed on the console.
+
+For example, if you enter 1 5 as coordinates for the first line and 3 7 for the second line, the program will output true because the lines overlap.
+
+
+
+
--- Q1/Test_Q1.java
+++ Q1/Test_Q1.java
@@ -7,8 +7,22 @@ public class Test_Q1 {
         int x3 = line2[0];
         int x4 = line2[1];
 
+        // Check if a line is a point
+        if(x1==x2 || x3==x4)
+        {
+        	System.out.println("\nGiven Case is Invalid... One input is a point");
+        	return false;
+        }
+        
+        // Check if wrong coordinates are given
+        if(x1>x2 || x3>x4)
+        {
+        	System.out.println("\nGiven Case is Invalid... Starting point is greater than ending point");
+        	return false;
+        }
+        
         // Check if the lines overlap
-        if (x1 <= x4 && x2 >= x3) {
+        if (x1 < x4 && x2 > x3) {
             return true;
         } else {
             return false;
@@ -28,6 +42,25 @@ public class Test_Q1 {
         
         boolean overlap1 = doLinesOverlap(line1, line2);
         System.out.println(overlap1);
+        
+        System.out.println("\nFollowing are some test cases\n");
+        int[][] testCases = {
+                {1, 5, 3, 7},      // Test Case 1: Overlapping lines
+                {1, 3, 4, 6},      // Test Case 2: Non-overlapping lines
+                {1, 5, 5, 7},      // Test Case 3: Meeting at endpoint
+                {-3, 2, -1, 4},    // Test Case 4: Negative coordinates, overlap
+                {1000000, 1000000000, 999999999, 1000000001}, // Test Case 5: Large coordinates, overlap
+                {2, 2, 2, 2},      // Test Case 6: Same coordinates for both lines
+                {1, 5, 5, 5},      // Test Case 7: Invalid input, same coordinate for different lines
+              	{5, 1, 1, 5}       // Test Case 8: Invalid input, reversed coordinates for same line
+            };
+
+            for (int i = 0; i < testCases.length; i++) {
+                int[] line11 = {testCases[i][0], testCases[i][1]};
+                int[] line22 = {testCases[i][2], testCases[i][3]};
+                boolean overlap = doLinesOverlap(line11, line22);
+                System.out.println("Test Case " + (i + 1) + ": " + overlap);
+            }
     }
 }
 
